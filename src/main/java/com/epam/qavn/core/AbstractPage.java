e2f0c10ee41f881dc6a8f7c463e7ec64d88d5284
DucLe8 <105845413+DucLe8@users.noreply.github.com>
2023-02-08 16:48:34 +0700

    Feat: implement AbstractPage Class (#4)
    * feat: implement AbstractPage and LoginPage
    
    * implement AbstractPage and LoginPage
    
    * feat: implement AbstractPage Class
    
    ---------
    
    Co-authored-by: Duc Le <duclh8@gmail.com>

--- src/main/java/com/epam/qavn/core/AbstractPage.java
+++ src/main/java/com/epam/qavn/core/AbstractPage.java
@@ -1,4 +1,130 @@
 package com.epam.qavn.core;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+import static com.epam.qavn.constant.DefaultConfig.SHORT_TIME_OUT;
+import static java.time.Duration.ofMillis;
+import static org.openqa.selenium.interactions.PointerInput.Kind.TOUCH;
+import static org.openqa.selenium.interactions.PointerInput.MouseButton.LEFT;
+import static org.openqa.selenium.interactions.PointerInput.Origin.viewport;
+
 public class AbstractPage {
+    private final PointerInput FINGER = new PointerInput(TOUCH, "finger");
+
+    public WebElement findElementBy(AppiumDriver driver, By by) {
+        waitUntilElementVisible(driver, by, Duration.ofSeconds(SHORT_TIME_OUT));
+        return driver.findElement(by);
+    }
+
+    public List<WebElement> findElementsBy(AppiumDriver driver, By by) {
+        waitUntilElementVisible(driver, by, Duration.ofSeconds(SHORT_TIME_OUT));
+        return driver.findElements(by);
+    }
+
+    public String getElementText(AppiumDriver driver, By by) {
+        return findElementBy(driver, by).getText();
+    }
+
+    public void inputText(AppiumDriver driver, By by, String text) {
+        findElementBy(driver, by).sendKeys(text);
+    }
+
+    public void tapCenterOf(AppiumDriver driver, WebElement element, Duration duration) {
+        Point point = getCenter(element);
+        Sequence tap = new Sequence(FINGER, 1)
+                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), point.getX(), point.getY()))
+                .addAction(FINGER.createPointerDown(LEFT.asArg()))
+                .addAction(new Pause(FINGER, duration))
+                .addAction(FINGER.createPointerUp(LEFT.asArg()));
+        driver.perform(Collections.singletonList(tap));
+    }
+
+    public void tapByPoint(AppiumDriver driver, Point point, Duration duration) {
+        Sequence tap = new Sequence(FINGER, 1)
+                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), point.getX(), point.getY()))
+                .addAction(FINGER.createPointerDown(LEFT.asArg()))
+                .addAction(new Pause(FINGER, duration))
+                .addAction(FINGER.createPointerUp(LEFT.asArg()));
+        driver.perform(Collections.singletonList(tap));
+    }
+
+    public void doSwipe(AppiumDriver driver, Point start, Point end, Duration duration) {
+        Sequence swipe = new Sequence(FINGER, 1)
+                .addAction(FINGER.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
+                .addAction(FINGER.createPointerDown(LEFT.asArg()))
+                .addAction(FINGER.createPointerMove(duration, viewport(), end.getX(), end.getY()))
+                .addAction(FINGER.createPointerUp(LEFT.asArg()));
+        driver.perform(Collections.singletonList(swipe));
+    }
+
+    public void doSwipeHorizontalFromRight(AppiumDriver driver, WebElement element, Duration duration) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = element.getRect().x + (element.getSize().width * 0.9);
+        double endX = element.getRect().x + (element.getSize().width * 0.1);
+        doSwipe(driver, new Point((int) startX, centerY), new Point((int) endX, centerY), duration);
+    }
+
+    public void doSwipeHorizontalFromLeft(AppiumDriver driver, WebElement element, Duration duration) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        double startX = element.getRect().x + (element.getSize().width * 0.1);
+        double endX = element.getRect().x + (element.getSize().width * 0.9);
+        doSwipe(driver, new Point((int) startX, centerY), new Point((int) endX, centerY), duration);
+    }
+
+    public void doSwipeVerticalFromDown(AppiumDriver driver, WebElement element, Duration duration) {
+        int centerX = element.getRect().x + (element.getSize().width / 2);
+        double startY = element.getRect().y + (element.getSize().height * 0.9);
+        double endY = element.getRect().y + (element.getSize().height * 0.1);
+        doSwipe(driver, new Point(centerX, (int) startY), new Point(centerX, (int) endY), duration);
+    }
+
+    public void doSwipeVerticalFromUp(AppiumDriver driver, WebElement element, Duration duration) {
+        int centerX = element.getRect().x + (element.getSize().width / 2);
+        double startY = element.getRect().y + (element.getSize().height * 0.1);
+        double endY = element.getRect().y + (element.getSize().height * 0.9);
+        doSwipe(driver, new Point(centerX, (int) startY), new Point(centerX, (int) endY), duration);
+    }
+
+    public void dragAndDrop(AppiumDriver driver, WebElement start, WebElement end, Duration duration) {
+        doSwipe(driver, getCenter(start), getCenter(end), duration);
+    }
+
+    public void dragAndDrop(AppiumDriver driver, Point start, Point end, Duration duration) {
+        doSwipe(driver, start, end, duration);
+    }
+
+    public Point getCenter(WebElement element) {
+        int centerY = element.getRect().y + (element.getSize().height / 2);
+        int centerX = element.getRect().x + (element.getSize().width / 2);
+        return new Point(centerX, centerY);
+    }
+
+    public void waitUntilElementVisible(AppiumDriver driver, WebElement element, Duration duration) {
+        new WebDriverWait(driver, duration).until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitUntilElementVisible(AppiumDriver driver, By by, Duration duration) {
+        new WebDriverWait(driver, duration).until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public void waitUntilElementClickable(AppiumDriver driver, WebElement element, Duration duration) {
+        new WebDriverWait(driver, duration).until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitUntilElementClickable(AppiumDriver driver, AppiumBy by, Duration duration) {
+        new WebDriverWait(driver, duration).until(ExpectedConditions.elementToBeClickable(by));
+    }
 }
--- src/main/java/com/epam/qavn/pages/LoginPage.java
+++ src/main/java/com/epam/qavn/pages/LoginPage.java
@@ -1,6 +1,49 @@
 package com.epam.qavn.pages;
 
 import com.epam.qavn.core.AbstractPage;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.epam.qavn.constant.DefaultConfig.SHORT_PRESS_TIME;
+import static com.epam.qavn.constant.DefaultConfig.SHORT_TIME_OUT;
 
 public class LoginPage extends AbstractPage {
+    private static final String VALID_EMAIL = "abc@gmail.com";
+    private static final String VALID_PASSWORD = "12345678";
+    private final By menuLogin = AppiumBy.accessibilityId("Login");
+    private final By txtEmail = AppiumBy.accessibilityId("input-email");
+    private final By txtPassword = AppiumBy.accessibilityId("input-password");
+    private final By btnLogin = AppiumBy.accessibilityId("button-LOGIN");
+    private final By lblEmailErrorMessage = AppiumBy.xpath("//android.view.ViewGroup[./android.widget.EditText[@content-desc='input-email']]/following-sibling::android.widget.TextView[1]");
+    private final By lblPasswordErrorMessage = AppiumBy.xpath("//android.view.ViewGroup[./android.widget.EditText[@content-desc='input-password']]/following-sibling::android.widget.TextView[1]");
+
+    public LoginPage login(AppiumDriver driver, String email, String password) {
+        tapCenterOf(driver, findElementBy(driver, menuLogin), Duration.ofMillis(SHORT_PRESS_TIME));
+        waitUntilElementVisible(driver, txtEmail, Duration.ofSeconds(SHORT_TIME_OUT));
+        inputText(driver, txtEmail, email);
+        inputText(driver, txtPassword, password);
+        tapCenterOf(driver, findElementBy(driver, btnLogin), Duration.ofMillis(SHORT_PRESS_TIME));
+        return this;
+    }
+
+    public LoginPage loginWithEmptyAccount(AppiumDriver driver) {
+        login(driver, "", "");
+        return this;
+    }
+
+    public LoginPage loginWithValidAccount(AppiumDriver driver) {
+        login(driver, VALID_EMAIL, VALID_PASSWORD);
+        return this;
+    }
+
+    public String getEmailErrorMessage(AppiumDriver driver) {
+        return getElementText(driver, lblEmailErrorMessage);
+    }
+
+    public String getPasswordErrorMessage(AppiumDriver driver) {
+        return getElementText(driver, lblPasswordErrorMessage);
+    }
 }
